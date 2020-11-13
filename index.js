16e7b27e2a40af0d683576482e43e8bdb5c97c13
12wollmana <awollman2@gmail.com>
2020-11-13 14:31:39 -0600

    Routing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 //#region FILES
 
@@ -38,7 +39,18 @@ const http = require("http");
 //#region SERVER
 
 const server = http.createServer((req, res) => {
-    res.end("Hello from the server!");
+    const pathName = req.url;
+    if (pathName === "/" || pathName === "/overview") {
+        res.end("This is the overview!");
+    } else if (pathName === "/product") {
+        res.end("This is the product!");
+    } else {
+        res.writeHead(404, {
+            "Content-type": "text/html",
+            "my-own-header": "hello-world",
+        });
+        res.end("<h1>Page not found!</h1>");
+    }
 });
 
 const localhost = "127.0.0.1";
