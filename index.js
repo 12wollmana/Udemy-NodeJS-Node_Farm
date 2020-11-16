d509147ef3a2eb44bb074ce34846212d57e4c2cb
12wollmana <awollman2@gmail.com>
2020-11-16 14:48:06 -0600

    Using Modules 2: Our Own Modules

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
+const replaceTemplate = require(`./modules/replaceTemplate`);
+
 //---------------------------------------------
 //#region FILES
 
@@ -39,26 +41,6 @@ const url = require("url");
 
 //#############################################
 //#region SERVER
-
-const replaceTemplate = (temp, product) => {
-    let output = temp;
-
-    // Use regex to replace all
-    output = output.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if (!product.organic) {
-        output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-    }
-
-    return output;
-};
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
